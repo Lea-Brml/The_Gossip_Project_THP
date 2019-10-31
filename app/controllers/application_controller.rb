cb9a03257246a605838fed56b1d65d9c46175bc7
Lea-Brml <l.bramoulle@hotmail.fr>
2019-10-31 16:26:59 +0100

    pushez vous de là

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+
+  def compare_user(session_id, user_id)
+
+    if session_id == user_id
+      true
+    else
+      false
+    end
+
+  end
+
+
 end
